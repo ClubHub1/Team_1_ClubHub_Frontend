fd5bea98aa92830d8904ffaf7d88eb298904cd58
aween121 <123986179+aween121@users.noreply.github.com>
2025-12-04 19:02:41 +0000

    fixing login page

--- src/loginPage.vue
+++ src/loginPage.vue
@@ -77,67 +77,76 @@ async function handleSubmit() {
 <template>
   <v-app>
     <v-main>
-
       <v-container>
         <v-row justify="center">
-          <v-column >
+          <v-col align-self="end">
             <v-card class = "bg-primary mt-10" height="500" width="400">
               <v-card-title class="ml-3 mt-3 mb-10">
                 <h1>Welcome <br/>
                 Back.</h1>
               </v-card-title>
             </v-card>
-          </v-column>
-          <v-column>
-            <v-card class = "mt-10" height="500" width="400" text>
+          </v-col>
 
+          <v-col>
+            <v-card class = "mt-10" height="500" width="400" text>
               <v-card-title class="text-center">
                 <h1>Sign In</h1>
               </v-card-title>
 
               <v-container class="ml-3">
-                <v-form v-model="valid" class="mt-7">
-
-                  <v-row
-                  >
+                <!-- NOTE: @submit.prevent calls our handleSubmit -->
+                <v-form v-model="valid" class="mt-7" @submit.prevent="handleSubmit">
+                
+                  <v-row>
                     <v-text-field
                       v-model="email"
                       :rules="emailRules"
                       label="E-mail"
                       required
                       class="mr-6 mb-5"
-                    ></v-text-field>
+                    />
                   </v-row>
 
                   <v-row>
-                      <v-text-field
-                        v-model="password"
-                        :rules="passwordRules"
-                        label="Password"
-                        type ="password"
-                        required
-                        class="mr-6"
-                      ></v-text-field>
+                    <v-text-field
+                      v-model="password"
+                      :rules="passwordRules"
+                      label="Password"
+                      type="password"
+                      required
+                      class="mr-6"
+                    />
+                  </v-row>
+
+                  <!-- error from backend -->
+                  <v-row v-if="error" class="mt-3">
+                    <v-alert type="error" variant="tonal" class="mr-6">
+                      {{ error }}
+                    </v-alert>
                   </v-row>
 
                   <v-row class="justify-center">
-                    <v-btn 
+                    <v-btn
                       type="submit"
                       class="mt-7 bg-primary"
                       width="100"
+                      :loading="loading"
                     >
                       Login
                     </v-btn>
                   </v-row>
 
-                  <v-row class="justify-center mt-6">
-                    <p>Don't have an account? <router-link to="/register">Register Here!</router-link> </p>
+                  <v-row class="justify-center mt-6"> 
+                    <p>
+                      Don't have an account?
+                      <router-link to="/register">Register Here!</router-link>
+                    </p>
                   </v-row>
-
                 </v-form>
               </v-container>
             </v-card>
-          </v-column>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
